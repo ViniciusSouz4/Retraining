6e5599bc7cd4f2e38a6482dc7fe3ccaa180d30ab
ViniciusSouz4 <55293485+ViniciusSouz4@users.noreply.github.com>
2020-11-06 20:50:13 -0300

    Create Retraining1.cpp

---
+++ Retraining1.cpp
@@ -0,0 +1,47 @@
+/*
+
+* Retraining1
+
+* [ Funcionalidade ]
+
+* Este programa lê dois inteiros de entrada,
+
+calcula a soma entre os dois valores
+
+e apresenta o resultado na tela.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+  int num1, num2;
+
+  
+
+  // [ Receber os valores de entrada ]
+
+  cout << "Digite o primeiro valor:" << endl;
+
+  cin >> num1;
+
+  cout << "Digite o segundo valor:" << endl;
+
+  cin >> num2;
+
+  
+
+  // Somar os valores de entrada e mostrar o resultado:
+
+  cout << "Resultado: " << num1+num2 << endl;
+
+  
+
+  return 0;
+
+}
